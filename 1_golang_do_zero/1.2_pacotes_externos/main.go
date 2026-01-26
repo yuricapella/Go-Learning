14dfdb3599d80fa3df71477bd8493f5d4c24e8d3
yuricapella <yuri.udsantos@mercadolivre.com>
2026-01-26 14:32:44 -0300

    feat(pacotes_externos): adiciona codigo que mostra como adicionar um pacote externo e utiliza-lo

--- 1_golang_do_zero/1.2_pacotes_externos/main.go
+++ 1_golang_do_zero/1.2_pacotes_externos/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Pacote 1.2 - Pacotes Externos")
-
+	email := "teste@teste.com"
+	err := checkmail.ValidateFormat(email)
+	fmt.Println(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yuricapella/Go-Learning
 
 go 1.25.5
+
+require github.com/badoux/checkmail v1.2.4 // indirect
